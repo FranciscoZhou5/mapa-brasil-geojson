d3a35d45e0f1a01b9ff2b2b61ccf4e077960eee9
FranciscoZhou5 <fg45874587@proton.me>
2023-10-04 07:41:15 -0300

    feat: add Brazil as svg

---
+++ src/app/svg/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import BrazilSVG from "@/components/BrazilSVG";
+
+export default function BrazilInSVG() {
+  return (
+    <div className="w-screen min-h-screen flex justify-center items-center">
+      <BrazilSVG />
+    </div>
+  );
+}
